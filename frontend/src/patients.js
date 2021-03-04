7a256c77f118231b9c0d14085ddfe7e422352527
Eric Palmas <ericpalmas96@gmail.com>
2021-03-04 21:28:05 +0100

    home page

---
+++ frontend/src/patients.js
@@ -0,0 +1,52 @@
+const patients = [
+    {
+        _id: '1',
+        name: 'Marco',
+        surname: 'Rossi',
+        age: '47',
+        pathology: 'brief description of the pathology',
+        therapy: 'prescribed medications'
+    }, 
+    {
+        _id: '2',
+        name: 'Pietro',
+        surname: 'Neri',
+        age: '32',
+        pathology: 'brief description of the pathology',
+        therapy: 'prescribed medications'
+    }, 
+    {
+        _id: '3',
+        name: 'Massimo',
+        surname: 'Verdi',
+        age: '23',
+        pathology: 'brief description of the pathology',
+        therapy: 'prescribed medications'
+    }, 
+    {
+        _id: '4',
+        name: 'Luca',
+        surname: 'Bianchi',
+        age: '63',
+        pathology: 'brief description of the pathology',
+        therapy: 'prescribed medications'
+    }, 
+    {
+        _id: '5',
+        name: 'Aldo',
+        surname: 'Grasso',
+        age: '43',
+        pathology: 'brief description of the pathology',
+        therapy: 'prescribed medications'
+    }, 
+    {
+        _id: '6',
+        name: 'Giorgio',
+        surname: 'Magri',
+        age: '33',
+        pathology: 'brief description of the pathology',
+        therapy: 'prescribed medications'
+    }, 
+]
+
+export default patients
---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Row, Col, Button, InputGroup, FormControl } from 'react-bootstrap'
+import Patient from '../components/Patient'
+import patients from '../patients'
+
+const HomeScreen = () => {
+  return (
+    <>
+      <h1>List of patients</h1>
+
+      
+      {/* <InputGroup className="mb-3 mt-4" style={{  width: '20rem'}}>  
+          <FormControl
+             placeholder="Search"
+             aria-label="Recipient's username"
+             aria-describedby="basic-addon2"
+          />
+      </InputGroup> */}
+
+
+      <Row className='mt-4' style={{ float: 'left', display: 'inline-block' }}>
+        {patients.map(patient => (
+          <Col sm={12} md={6} lg={4}>
+            <Patient patient={patient} />
+          </Col>
+        ))}
+      </Row>
+
+      {/* <div className="mt-4" style={{ float: 'left', display: 'inline-block' }}>
+       <Button variant="primary" size="lg">
+          New patient
+       </Button>{' '}
+       <Button variant="primary" size="lg">
+          Remove patient
+       </Button>
+      </div> */}
+   
+    </>
+  )
+}
+
+export default HomeScreen
