1238a440ed6ff3236c6e574fce9f12506c494cf6
Eric Palmas <ericpalmas96@gmail.com>
2021-10-25 10:35:28 +0200

    responsive sidebar

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Row, Col, Card } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
-import SidebarNuova from '../components/SidebarNuova'
 
 const HomeScreen = () => {
   return (
