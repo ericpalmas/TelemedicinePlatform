733167c3583ba36b5cad1cc7fcd9315cec647050
Eric Palmas <ericpalmas96@gmail.com>
2022-02-08 21:43:38 +0100

    Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,9 @@ import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import colors from 'colors'
+import path from 'path'
+import morgan from 'morgan'
+
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 import patientRoutes from './routes/patientRoutes.js'
@@ -22,6 +25,10 @@ connectDB()
 
 const app = express()
 
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
 // with this instruction i can pass json to the server
 app.use(express.json())
 
@@ -37,6 +44,20 @@ app.use('/api/responses', responseRoutes)
 app.use('/api/timeSlot', timeSlotRoutes)
 app.use('/api/studies', studyRoutes)
 
+const __dirname = path.resolve()
+if (process.env.NODE_ENV === 'production') {
+  // we set the build folder as a static folder
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  // any route that is not defined before point to the file index.html
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...')
+  })
+}
 app.use(notFound)
 app.use(errorHandler)
 
